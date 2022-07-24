7f5fb22a67d396ae69b284097e5afa5609fb7d44
Manuel Sanson <msanson.programacion@gmail.com>
2022-07-24 17:54:34 -0300

    formula calculo de pesos y clase jugador

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // Funcion login y bienvenida
-
 const bienvenidaUsuario = (username, password) => {
     alert('Login exitoso. Bienvenido ' + username)
 }
@@ -9,13 +8,26 @@ const password = prompt('Ingresa tu contraseña')
 bienvenidaUsuario(username, password)
 
 
-
 // Funcion calculo de peso maximo
 const calcularRM = (peso, numeroReps) => {
-    let rm = peso * numeroReps
-    alert('Tu RM en este ejercicio es ' + rm)
+    let rm = peso / (1.0278 - 0.0278 * numeroReps)
+    alert('Tu RM en ' + ejercicio + ' es ' + rm)
 }
 
+const ejercicio = prompt('Selecciona el ejercicio (Sentadillas, Peso Muerto, Dominadas o Pecho Plano)')
 const peso = prompt('Ingresa el peso')
 const numeroReps = prompt('Ingresa el número de repeticiones')
 calcularRM(peso, numeroReps)
+
+
+// Datos del jugador
+class Jugador{
+    constructor(nombreCompleto, division, edad, altura, peso, posicion){
+        this.nombreCompleto = nombreCompleto
+        this.division = division
+        this.edad = edad
+        this.altura = altura
+        this.peso = peso
+        this.posicion = posicion
+    }
+}
