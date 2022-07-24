872abdec9ea5d1bcee3e62357692900b2ac7084e
Manuel Sanson <msanson.programacion@gmail.com>
2022-07-24 13:22:58 -0300

    arreglos login y funciones js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
+// Funcion login y bienvenida
+
 const bienvenidaUsuario = (username, password) => {
     alert('Login exitoso. Bienvenido ' + username)
 }
 
-
-const username = prompt('Ingrese su username')
-const password = prompt('Ingrese su password')
+const username = prompt('Ingresa tu nombre de usuario')
+const password = prompt('Ingresa tu contraseña')
 bienvenidaUsuario(username, password)
+
+
+
+// Funcion calculo de peso maximo
+const calcularRM = (peso, numeroReps) => {
+    let rm = peso * numeroReps
+    alert('Tu RM en este ejercicio es ' + rm)
+}
+
+const peso = prompt('Ingresa el peso')
+const numeroReps = prompt('Ingresa el número de repeticiones')
+calcularRM(peso, numeroReps)
