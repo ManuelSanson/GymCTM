31182ccfecb63cdf40c3451fd39bd7818e4fd028
Manuel Sanson <msanson.programacion@gmail.com>
2022-08-08 15:21:29 -0300

    correcciones

--- js/app.js
+++ js/app.js
@@ -1,51 +1,11 @@
 // Funcion login y bienvenida
-/*
 const bienvenidaUsuario = (username, password) => {
-    alert('Login exitoso. Bienvenido ' + username)
+    console.log('Login exitoso. Bienvenido ' + username)
 }
 
 const username = prompt('Ingresa tu nombre de usuario')
 const password = prompt('Ingresa tu contraseña')
 bienvenidaUsuario(username, password)
-*/
-
-// Datos de los jugadores
-/*
-const jugadores = [
-    {nombreCompleto: 'Manuel Sanson', division: 'Primera', edad: 27, altura: 168, peso: 70, posicion: 'Medioscrum'},
-    {nombreCompleto: 'Juan Pedro Torres', division: 'Primera', edad: 28, altura: 175, peso: 82, posicion: 'Centro'},
-    {nombreCompleto: 'Facundo Suarez', division: 'Primera', edad: 30, altura: 182, peso: 120, posicion: 'Pilar'},
-]
-
-jugadores.push({nombreCompleto: 'Ignacio Iltis', division: 'Primera', edad: 23, altura: 178, peso: 75, posicion: 'Fullback'})
-console.log(jugadores)
-
-for (const jugador of jugadores) {
-    console.log(jugador.nombreCompleto, jugador.peso)
-}
-*/
-
-// Funcion para buscar jugadores por posicion
-/*
-const posicionJugador = jugadores.filter((jugador) => jugador.posicion == 'Pilar')
-console.log(posicionJugador)
-*/
-
-// Funcion calculo de peso maximo
-const calcularRM = (peso, numeroReps) => {
-    let rm = peso / (1.0278 - 0.0278 * numeroReps)
-    console.log('Tu RM en ' + ejercicio + ' es ' + rm)
-}
-
-const ejercicio = prompt('Selecciona el ejercicio (Sentadillas, Peso Muerto, Dominadas o Pecho Plano)').toLowerCase()
-
-if (ejercicio == 'sentadillas' || ejercicio == 'peso muerto' || ejercicio == 'dominadas' || ejercicio == 'pecho plano') {
-    const peso = prompt('Ingresa el peso')
-    const numeroReps = prompt('Ingresa el número de repeticiones')
-    calcularRM(peso, numeroReps)
-} else {
-    alert('No elegiste correctamente el ejercicio')
-}
 
 
 //Template literal de los ejercicios
@@ -83,7 +43,6 @@ ejercicios.forEach((ejercicio) => {
 
 //Buscador de ejercicios
 const buscadorEjercicio = document.querySelector('#buscadorEjercicio')
-console.log(buscadorEjercicio)
 
 const buscarEjercicio = () => {
     document.querySelectorAll('.ejercicio').forEach(ejercicio => ejercicio.remove())
@@ -97,9 +56,9 @@ const buscarEjercicio = () => {
             <article class="ejercicio">
                 <h3>${ejercicio.nombre}</h3>
                 <h6>Peso (kg)</h6>
-                <input type="Peso">
+                <input type="text" id="peso${ejercicio.nombre}">
                 <h6>Repeticiones</h6>
-                <input type="Repeticiones">
+                <input type="text" id="repeticiones${ejercicio.nombre}">
                 <h4>RM (kg): Resultado</h4>
             </article>
         `
@@ -109,3 +68,20 @@ const buscarEjercicio = () => {
 } 
 
 buscadorEjercicio.addEventListener('input', buscarEjercicio)
+
+
+// Funcion calculo de peso maximo
+const calcularRM = (peso, numeroReps) => {
+    let rm = peso / (1.0278 - 0.0278 * numeroReps)
+    console.log('Tu RM en ' + ejercicio + ' es ' + rm)
+}
+
+// const ejercicio = prompt('Selecciona el ejercicio (Sentadillas, Peso Muerto, Dominadas o Pecho Plano)').toLowerCase()
+
+// if (ejercicio == 'sentadillas' || ejercicio == 'peso muerto' || ejercicio == 'dominadas' || ejercicio == 'pecho plano') {
+//     const peso = prompt('Ingresa el peso')
+//     const numeroReps = prompt('Ingresa el número de repeticiones')
+//     calcularRM(peso, numeroReps)
+// } else {
+//     alert('No elegiste correctamente el ejercicio')
+// }
